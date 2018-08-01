dcf0102174a6bf6c7ad793f12f693db7654f1a1b
Rubayet Hossain <rubayet.hossain@mail.mcgill.ca>
2018-08-01 16:49:34 +0600

    Update mario.c

--- mario.c
+++ mario.c
@@ -7,17 +7,17 @@ int main (void){
     do{
         printf("Enter the height of the pyramid");
 
-      n= get_int ();
+      n= get_int (); //ask user for the input
     }
     while (n<0 || n>23);
 
     for (int i=1; i<=n; i++){
-        for (int j=n-i; j>0; j--){
+        for (int j=n-i; j>0; j--){ // creating spaces
             printf(" ");
 
 
         }
-        for (int k=1; k<=i+1; k++){
+        for (int k=1; k<=i+1; k++){ // creating hashes 
 
              printf("#");
 
